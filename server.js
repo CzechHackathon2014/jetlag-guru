88a5fe1526a239eca950ef025069f3f390636b09
Raphael Gabbarelli <raphael.gabbarelli@concur.com>
2014-11-08 14:07:29 +0100

    IATA code search should work

--- server.js
+++ server.js
@@ -10,15 +10,10 @@ var async = require('async');
 var socketio = require('socket.io');
 var express = require('express');
 
-//
-// ## SimpleServer `SimpleServer(obj)`
-//
-// Creates a new instance of SimpleServer with the following options:
-//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
-//
+var airports = require("./airports.json");
+
 var router = express();
 var server = http.createServer(router);
-// var io = socketio.listen(server);
 
 router.use(express.static(path.resolve(__dirname, 'client')));
 
@@ -26,70 +21,23 @@ router.get('/test', function(req, res) {
 	res.json({ message: 'hooray! welcome to our api!' });	
 });
 
-
-
-
-
-// var messages = [];
-// var sockets = [];
-
-// io.on('connection', function (socket) {
-//     messages.forEach(function (data) {
-//       socket.emit('message', data);
-//     });
-
-//     sockets.push(socket);
-
-//     socket.on('disconnect', function () {
-//       sockets.splice(sockets.indexOf(socket), 1);
-//       updateRoster();
-//     });
-    
-//     socket.on('search', function(msg){
-//       socket.emit("Ahoj");
-//     });
-
-//     socket.on('message', function (msg) {
-//       var text = String(msg || '');
-
-//       if (!text)
-//         return;
-
-//       socket.get('name', function (err, name) {
-//         var data = {
-//           name: name,
-//           text: text
-//         };
-
-//         broadcast('message', data);
-//         messages.push(data);
-//       });
-//     });
-
-//     socket.on('identify', function (name) {
-//       socket.set('name', String(name || 'Anonymous'), function (err) {
-//         updateRoster();
-//       });
-//     });
-//   });
-
-// function updateRoster() {
-//   async.map(
-//     sockets,
-//     function (socket, callback) {
-//       socket.get('name', callback);
-//     },
-//     function (err, names) {
-//       broadcast('roster', names);
-//     }
-//   );
-// }
-
-// function broadcast(event, data) {
-//   sockets.forEach(function (socket) {
-//     socket.emit(event, data);
-//   });
-// }
+router.get('/airportSearch', function(req, res){  
+  if(req.query.search != null)
+  {
+    var airport = airports[req.query.search.toUpperCase()]; //IATA case
+    if(airport != null){
+    	
+    	var airportDetails = {
+    		IATA : 	req.query.search.toUpperCase(),
+    		TimeZone : airport[2],
+    		Name : airport[0],
+    		City : airport[1]
+    	};
+    	
+    	res.json(airportDetails);
+    }
+  }
+});
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
